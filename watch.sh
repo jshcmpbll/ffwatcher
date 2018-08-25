d8092dc88b32b268a8adfe942259a71d40da7ded
Joshua Campbell <me@joshuadcampbell.com>
2018-08-25 16:27:53 -0700

    Converted File paths to variables within the code.

--- watch.sh
+++ watch.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
 # Variables for the FFMPEG watch folder:
+
 WATCHFOLDER=~/Files/Convert
+WATCHSCRIPTDIR=~/.watch
+
 FFMPEGSCRIPT=~/.ffmpeg/./MP4.sh
+FFMPEG_OPTIONS=No_OPTIONS
+
 DELAY=20
 
 
 while true
 do
 	# Moves the current file list to the past file list. 
-	mv ~/.watch/current ~/.watch/past
+	mv $WATCHSCRIPTDIR/current $WATCHSCRIPTDIR/past
 	
 	# Gets current list form the working directoy and writes it to the file called current 	
-	ls -1A $WATCHFOLDER > ~/.watch/current
+	ls -1A $WATCHFOLDER > $WATCHSCRIPTDIR/current
 
 	# Compares the two documents "curent" and "past" and adds file paths to document called "NewFiles"
-	diff --changed-group-format='%>' --unchanged-group-format='' ~/.watch/past ~/.watch/current > ~/.watch/NewFiles
+	diff --changed-group-format='%>' --unchanged-group-format='' $WATCHSCRIPTDIR/past $WATCHSCRIPTDIR/current > $WATCHSCRIPTDIR/NewFiles
 	
 	while read line
 	do
 		# Tests to see if the new item is a file or directory
-		if test -f /media/sdv1/camplab/Files/Convert/$line
+		if test -f $WATCHFOLDER/$line
 		then                                   
 			# This is where the actual part of video conversion happends. You can replace this next line with the following
 			# "ffmpeg -i $line [output file options] $( basename "$file" ) += ".mp4""
 			# Alternatively, use the line below which makes a call for a new script that will handle file naming and output 			  file options
-			~/.ffmpeg/./MP4.sh ~/Files/Convert/$line 
+			$FFMPEGSCRIPT $WATCHFOLDER/$line 
 		
 			echo $line > delete
 			# This is not working right now but will in the future. The idea is that the file path is added to a new                                  documentt where another process will delete old files from the watch folder
 
 		else
 			# This needs work.. not sure what my intentions were with this one
-			mkdir ~/.watch/$line
+			mkdir $WATCHSCRIPTDIR/$line
 		fi
 
 
-	done <~/.watch/NewFiles
+	done <$WATCHSCRIPTDIR/NewFiles
 
 	# This is the delay in watch folder checks. Please assign above
 	sleep $DELAY
